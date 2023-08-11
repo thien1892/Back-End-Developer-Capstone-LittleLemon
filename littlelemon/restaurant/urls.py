b78abd20adc45178eac6b440d816c81522e44c65
thien1892 <thien1892@gmail.com>
2023-08-11 16:36:10 +0700

    add serializer and api

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -3,4 +3,18 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('menu-items', views.MenuItemsView.as_view()),
+    path('menu-items/<int:pk>', views.SingleMenuItemsView.as_view()),
+    path('booking/tables', views.BookingViewSet.as_view({'get': 'list',
+                                                         'post':'create',
+                                                        #  'put': 'update',
+                                                        #  'delete': 'destroy',
+                                                        #  'patch': 'partial_update',
+                                                         })),
+    path('booking/tables/<int:pk>', views.BookingViewSet.as_view({'get': 'retrieve',
+                                                        #  'post':'create',
+                                                         'put': 'update',
+                                                         'delete': 'destroy',
+                                                         'patch': 'partial_update',
+                                                         })),
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .models import Booking, Menu
+from .serializers import MenuSerializer, BookingSerializer
+from rest_framework import viewsets
 # Create your views here.
 
 def index(request):
     return render(request, 'index_lab.html', {})
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemsView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
